e6b1f790674ae98253fb8abd216ab266b245ecc3
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-12-03 15:25:12 +0100

    Ex 102 completed

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -3,42 +3,53 @@ package it.unibo.mvc;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    private static final String PATH = "src/main/resources/config.yml";
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
 
     /**
      * @param views
-     *            the views to attach
+     *              the views to attach
      */
     public DrawNumberApp(final DrawNumberView... views) {
         /*
          * Side-effect proof
          */
+        final Map<String, Integer> map = new HashMap<>();
         this.views = Arrays.asList(Arrays.copyOf(views, views.length));
-        for (final DrawNumberView view: views) {
+        for (final DrawNumberView view : views) {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
+            reader.lines().forEach(e -> {
+                String[] arr = e.split(": ");
+                map.put(arr[0], Integer.parseInt(arr[1]));
+            });
+        } catch (Exception e) {
+        }
+        this.model = new DrawNumberImpl(map.getOrDefault("minimum", 0), map.getOrDefault("maximum", 0),
+                map.getOrDefault("attemps", 0));
     }
 
     @Override
     public void newAttempt(final int n) {
         try {
             final DrawResult result = model.attempt(n);
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.result(result);
             }
         } catch (IllegalArgumentException e) {
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.numberIncorrect();
             }
         }
@@ -62,11 +73,12 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
 
     /**
      * @param args
-     *            ignored
-     * @throws FileNotFoundException 
+     *             ignored
+     * @throws FileNotFoundException
      */
     public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+        new DrawNumberApp(new DrawNumberViewImpl(), new DrawNumberViewImpl(), new PrintStreamView(System.out),
+                new PrintStreamView("output.log"));
     }
 
 }
